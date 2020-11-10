048111051a984376e2e1b5ededc3c21d7a8e96b6
owenglahn <orglahn@gmail.com>
2020-11-09 23:44:41 -0500

    Pass initial tests for P1 and P2

---
+++ HW_Sched.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Assignment implements Comparator<Assignment>{
+	int number;
+	int weight;
+	int deadline;
+	
+	
+	protected Assignment() {
+	}
+	
+	protected Assignment(int number, int weight, int deadline) {
+		this.number = number;
+		this.weight = weight;
+		this.deadline = deadline;
+	}
+	
+	
+	
+	/**
+	 * This method is used to sort to compare assignment objects for sorting. 
+	 */
+	@Override
+	public int compare(Assignment a1, Assignment a2) {
+		// TODO Implement this
+		
+		
+		return 0;
+	}
+}
+
+public class HW_Sched {
+	ArrayList<Assignment> Assignments = new ArrayList<Assignment>();
+	int m;
+	int lastDeadline = 0;
+	
+	protected HW_Sched(int[] weights, int[] deadlines, int size) {
+		for (int i=0; i<size; i++) {
+			Assignment homework = new Assignment(i, weights[i], deadlines[i]);
+			this.Assignments.add(homework);
+			if (homework.deadline > lastDeadline) {
+				lastDeadline = homework.deadline;
+			}
+		}
+		m =size;
+	}
+	
+	
+	/**
+	 * 
+	 * @return Array where output[i] corresponds to the assignment 
+	 * that will be done at time i.
+	 */
+	public int[] SelectAssignments() {
+		//TODO Implement this
+		
+		//Sort assignments
+		//Order will depend on how compare function is implemented
+		Collections.sort(Assignments, new Assignment());
+		
+		// If homeworkPlan[i] has a value -1, it indicates that the 
+		// i'th timeslot in the homeworkPlan is empty
+		//homeworkPlan contains the homework schedule between now and the last deadline
+		int[] homeworkPlan = new int[lastDeadline];
+		for (int i=0; i < homeworkPlan.length; ++i) {
+			homeworkPlan[i] = -1;
+		}
+		
+		
+		
+		
+		
+	
+		
+		return homeworkPlan;
+	}
+}
+	
+
+
+
---
+++ Kruskal.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Kruskal{
+
+    public static WGraph kruskal(WGraph g){
+
+        /* Fill this method (The statement return null is here only to compile) */
+    	WGraph toReturn = new WGraph();
+    	DisjointSets gDisJoint = new DisjointSets(g.getNbNodes());
+        for ( Edge e : g.listOfEdgesSorted())
+        {
+        	if (IsSafe(gDisJoint, e))
+        	{
+        		toReturn.addEdge(e);
+        		gDisJoint.union(e.nodes[0], e.nodes[1]);
+        	}
+        }
+        return toReturn;
+    }
+
+    public static Boolean IsSafe(DisjointSets p, Edge e){
+
+        /* Only need to return false if e is in the set, because kruskal() uses a sorted ArrayList of edges */
+    	if (p.find(e.nodes[0]) == p.find(e.nodes[1])) return false;
+        return true;
+    
+    }
+
+    public static void main(String[] args){
+
+        String file = args[0];
+        WGraph g = new WGraph(file);
+        WGraph t = kruskal(g);
+        System.out.println(t);
+
+   } 
+}
